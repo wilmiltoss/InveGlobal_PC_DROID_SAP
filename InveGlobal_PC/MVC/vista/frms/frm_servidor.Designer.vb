4435843fbeea552cbc9812ba49d8c88a184512ec
Wilson Miltos <32781341+wilmiltoss@users.noreply.github.com>
2021-08-23 07:48:04 -0400

    Integracion a Sap

---
+++ InveGlobal_PC/MVC/vista/frms/frm_servidor.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_servidor
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.rbEme = New System.Windows.Forms.RadioButton()
+        Me.rbSap = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(46, 117)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(90, 34)
+        Me.btnGuardar.TabIndex = 0
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(167, 117)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(92, 34)
+        Me.btnCancelar.TabIndex = 1
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'rbEme
+        '
+        Me.rbEme.AutoSize = True
+        Me.rbEme.Location = New System.Drawing.Point(90, 54)
+        Me.rbEme.Name = "rbEme"
+        Me.rbEme.Size = New System.Drawing.Size(48, 17)
+        Me.rbEme.TabIndex = 2
+        Me.rbEme.TabStop = True
+        Me.rbEme.Text = "EME"
+        Me.rbEme.UseVisualStyleBackColor = True
+        '
+        'rbSap
+        '
+        Me.rbSap.AutoSize = True
+        Me.rbSap.Location = New System.Drawing.Point(90, 77)
+        Me.rbSap.Name = "rbSap"
+        Me.rbSap.Size = New System.Drawing.Size(46, 17)
+        Me.rbSap.TabIndex = 3
+        Me.rbSap.TabStop = True
+        Me.rbSap.Text = "SAP"
+        Me.rbSap.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(62, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(166, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Seleccione el Servidor"
+        '
+        'frm_servidor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(287, 188)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.rbSap)
+        Me.Controls.Add(Me.rbEme)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Name = "frm_servidor"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Configurar Servidor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents rbEme As RadioButton
+    Friend WithEvents rbSap As RadioButton
+    Friend WithEvents Label1 As Label
+End Class
---
+++ InveGlobal_PC/MVC/vista/frms/frm_servidor.vb
@@ -0,0 +1,89 @@
+﻿Public Class frm_servidor
+
+
+    Public Const NOMBRE_CLASE As String = "frm_servidor"
+    'administradores de frm
+    Public oApControlador As ApControlador
+    'administradores de datos
+    Public oApModelo As ApModelo
+
+
+#Region "CONTRUCTORES "
+
+    Public Sub New(ByRef oApControladorParam As ApControlador)
+        oApControlador = oApControladorParam
+
+        InitializeComponent()
+
+
+    End Sub
+
+
+#End Region
+
+
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        'cerramos el este formulario
+        Me.Close()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        __guardar_datos()
+        'cerramos el formulario frmServidor
+        Me.Close()
+        oApControlador.notificar_mensaje_cierre("Cierre la aplicación para establecer los cambios", "Datos de Sistema")
+
+    End Sub
+
+    ''' <summary>
+    ''' CARGA el "Local a cambiar"
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub __guardar_datos()
+
+        Dim NOMBRE_METODO As String = NOMBRE_CLASE & ".__guardar_datos()"
+        Dim lResultado As New List(Of Object)(New Object() {0, NOMBRE_METODO & " No Ejecutado!"})
+
+        Try
+            'Cambio de sistema al EME
+            If rbEme.Checked = True Then
+                lResultado = oApControlador.oApModelo.Locales_ADM().lActualizar(Me.oApControlador.Local_OTD)
+                'si se ejecuto correctamente
+                If lResultado(0).Equals(1) Then
+
+                    'mensaje exitoso
+                    oApControlador.notificar_exito("Datos de Sistema guardados correctamente!", "Datos de Sistema")
+                Else
+                    'sino, mensaje de notificacion de error
+                    oApControlador.notificar_error(lResultado(1).ToString(), "Datos de Sistema")
+                End If
+
+                'Cambio de sistema al SAP
+            ElseIf rbSap.Checked = True Then
+                lResultado = oApControlador.oApModelo.Locales_ADM().lAgregar(Me.oApControlador.Local_OTD)
+
+                'si se ejecuto correctamente
+                If lResultado(0).Equals(1) Then
+
+                    'mensaje exitoso
+                    oApControlador.notificar_exito("Datos de Sistema guardados correctamente!", "Datos de Sistema")
+
+                Else
+                    'sino, mensaje de notificacion de error
+                    oApControlador.notificar_error(lResultado(1).ToString(), "Datos de Sistema")
+                End If
+
+            End If
+
+        Catch ex As Exception
+            'en caso de error, mensaje de notificacion
+            oApControlador.notificar_error(NOMBRE_METODO & " Error: " & ex.Message, "Datos de Sistema")
+
+        End Try
+
+        Cursor.Current = Cursors.Default
+
+    End Sub
+
+End Class
